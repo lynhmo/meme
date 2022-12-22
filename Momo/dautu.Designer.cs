05620905c393d6f53a0413f349778eb27db87600
Lynh <chinsugaming@gmail.com>
2022-12-23 06:39:44 +0700

    yes dau tu

--- Momo/dautu.Designer.cs
+++ Momo/dautu.Designer.cs
@@ -28,29 +28,297 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.table_all_Bank = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dautu_phantram = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.table_all_Bank)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 22);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(16, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 46);
+            this.label2.Size = new System.Drawing.Size(109, 37);
             this.label2.TabIndex = 3;
             this.label2.Text = "Đầu Tư";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(23, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "1 Tháng";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(226, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "3 Tháng";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(23, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "6 Tháng";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(226, 172);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(197, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "1 Năm";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label1.Location = new System.Drawing.Point(18, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thời gian gửi";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(23, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(197, 43);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "5 Năm";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button8.Location = new System.Drawing.Point(226, 221);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(197, 43);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "10 Năm";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // table_all_Bank
+            // 
+            this.table_all_Bank.AllowUserToAddRows = false;
+            this.table_all_Bank.AllowUserToDeleteRows = false;
+            this.table_all_Bank.AllowUserToResizeRows = false;
+            this.table_all_Bank.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.table_all_Bank.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.table_all_Bank.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.table_all_Bank.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table_all_Bank.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dautu_phantram});
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(45)))), ((int)(((byte)(139)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.table_all_Bank.DefaultCellStyle = dataGridViewCellStyle7;
+            this.table_all_Bank.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(0)))), ((int)(((byte)(100)))));
+            this.table_all_Bank.Location = new System.Drawing.Point(453, 121);
+            this.table_all_Bank.Name = "table_all_Bank";
+            this.table_all_Bank.RowHeadersVisible = false;
+            this.table_all_Bank.Size = new System.Drawing.Size(346, 351);
+            this.table_all_Bank.TabIndex = 8;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "id_bank";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Mã ngân hàng";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "bankname";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Tên ngân hàng";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dautu_phantram
+            // 
+            this.dautu_phantram.DataPropertyName = "dautu_phantram";
+            this.dautu_phantram.HeaderText = "Lãi xuất";
+            this.dautu_phantram.Name = "dautu_phantram";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label3.Location = new System.Drawing.Point(18, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Số tiền gửi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.textBox1.Location = new System.Drawing.Point(23, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 34);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label4.Location = new System.Drawing.Point(18, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Thông điệp";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.textBox2.Location = new System.Drawing.Point(23, 366);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 34);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.button9.Location = new System.Drawing.Point(287, 447);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 43);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "Gửi";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label5.Location = new System.Drawing.Point(18, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 28);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Thời hạn";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.textBox3.Location = new System.Drawing.Point(111, 407);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 34);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.label6.Location = new System.Drawing.Point(448, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 28);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Danh sách ngân hàng";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.checkBox1.Location = new System.Drawing.Point(21, 447);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 32);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Đáo hạn";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // dautu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(214)))), ((int)(((byte)(231)))));
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.table_all_Bank);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "dautu";
-            this.Size = new System.Drawing.Size(1121, 688);
+            this.Size = new System.Drawing.Size(841, 559);
+            this.Load += new System.EventHandler(this.dautu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table_all_Bank)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +327,25 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.DataGridView table_all_Bank;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dautu_phantram;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- Momo/dautu.cs
+++ Momo/dautu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace Momo
         public dautu()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dautu_Load(object sender, EventArgs e)
+        {
+            loadTableALL_Bank();
+        }
+        private static string connectionString = "Data Source=(local); Initial Catalog = momo; Integrated Security=SSPI;";
+        private static SqlConnection cnn = new SqlConnection(connectionString);
+        private void loadTableALL_Bank()
+        {
+            cnn.Open();
+            string query = $"SELECT * FROM bank";
+            SqlDataAdapter da = new SqlDataAdapter(query, cnn);
+            DataTable dtbl = new DataTable();
+            da.Fill(dtbl);
+            table_all_Bank.DataSource = dtbl;
+            cnn.Close();
         }
     }
 }
